fed005e4d40db2555aa84509ecf7d156891b0c3b
Adam Matthiesen <amatthiesen@outlook.com>
2024-06-25 01:00:47 -0700

    more unocss cleanup

--- uno.config.ts
+++ uno.config.ts
@@ -2,94 +2,13 @@ import { defineConfig, e, presetAttributify, presetTagify } from 'unocss'
 import { presetDaisy } from '@matthiesenxyz/unocss-preset-daisyui';
 import { presetIcons, presetTypography, presetUno, presetWind, presetWebFonts, transformerDirectives } from 'unocss';
 import type { IconifyJSON } from '@iconify-json/logos/index.js';
+import { basePreflight } from './unocss-preflight';
 
 const darkmodeSelector = { dark: '[data-theme="dark"]' }
 
 export default defineConfig({
     preflights: [
-        { getCSS() {
-             return `
-                *,::before,::after {
-                    @media screen and (max-width: 1024px) {
-                        .rank .ranklabel {
-                            font-size: 1.125rem; /* 18px */
-                            line-height: 1.75rem; /* 28px */
-                            flex-shrink: 1;
-                            padding-left: 0;
-                            padding-right: 0;
-                        }
-                        .rank .rankvalue {
-                            flex-shrink: 1;
-                            padding-left: 0;
-                            padding-right: 0;
-                        }
-                        .rank .levelnumber {
-                            font-size: 1.125rem; /* 18px */
-                            line-height: 1.75rem; /* 28px */
-                            flex-shrink: 1;
-                            padding-left: 0;
-                            padding-right: 0;
-                        }
-                        .rank .levelprogress {
-                            flex-shrink: 1;
-                            padding-left: 0;
-                            padding-right: 0;
-                        }
-                        .rank {
-                            font-size: 1.125rem; /* 18px */
-                            line-height: 1.75rem; /* 28px */
-                            display: flex;
-                            flex-direction: row;
-                            justify-content: center;
-                            align-content: center;
-                            align-items: center;
-                            padding-left: 0;
-                            padding-right: 0;
-                            gap: 0.5rem; /* 8px */
-                            width: fit-content;
-                        }
-                    }
-
-                    @media screen and (min-width: 768px) {
-                        .ranklabel {
-                            display: none;
-                        }
-                        .menu-divider {
-                            display: inherit;
-                            position: relative;
-                            padding: 0;
-                            margin: 0;
-                        }
-                    }
-
-                    @media screen and (max-width: 768px) {
-                        .menu-divider {
-                            display: none;
-                        }
-                    }
-
-                    profilelink {
-                        position: absolute;
-                        right: 2rem;
-                        top: 50%;
-                        vertical-align: middle;
-                    } 
-                    profilelink sl-tooltip, 
-                    profilelink sl-tooltip a {
-                        vertical-align: middle;
-                    }
-
-                    .progressbar {
-                        --height: 24px;
-                    }
-
-                    .progressbar::part(label) {
-                        font-weight: bolder;
-                        font-size: 1rem;
-                        text-align: center;
-                    }   
-                }` }
-        }
+        { getCSS() { return basePreflight } }
     ],
     shortcuts: {
         'main': 'flex flex-col justify-center justify-items-center max-w-[100vw] p-4 lg:p-12 bg-base-100 font-sans',
